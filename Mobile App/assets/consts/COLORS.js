d463d3be1c6316d82169d0707ff614ada6ee02f1
Ansam1999 <60790023+Ansam1999@users.noreply.github.com>
2021-07-02 18:00:53 +0300

    Add files via upload

---
+++ Mobile App/assets/consts/COLORS.js
@@ -0,0 +1,12 @@
+const COLORS = {
+    white: '#FFF',
+    dark: '#000',
+    primary: '#84d4a4',
+    secondary: '#7840A7',
+    light: '#f9f9f9',
+    grey: '#6D6C6C',
+    orange: '#f5a623',
+  };
+  
+  export default COLORS;
+  
